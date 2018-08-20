85d37f2969ba043a58718087071ad982e1638aa5
Ubuntu 18.04 <121478098@qq.com>
2018-08-20 04:03:29 -0700

    Add details

--- software_tools/data_flow_catcher/data_flow_catcher.py
+++ software_tools/data_flow_catcher/data_flow_catcher.py
@@ -3,9 +3,25 @@ Data flow catcher class defination
 """
 import os
 
+def getNICNames():
+    print("Choose the Network Interface Controller you will use:")
+    
+    dirNumber = 1
+    dirList = os.listdir("/sys/class/net")
+    for eachDir in dirList:
+        print(str(dirNumber) + ". " + eachDir)
+        dirNumber += 1 
+
+    dirChoice = int(input("Input the number:"))
+
+    return dirList[dirChoice - 1]
+
+
 class DataFlowCatcher(object):
     def __init__(self, current_time):
-        self.filename = str(current_time) + '.pcap'
+        self.filename = 'pcaps/' + current_time + '.pcap'
 
     def run(self):
-        os.system("sudo tcpdump -i wlx00117f139169 -G 600 -w "+ self.filename)
+        NICName = getNICNames()
+        print("Start Data Flow Catcher...\nIt will run in 1 minute...")
+        os.system("sudo tcpdump -i " + NICName + " -G 60 -W 1 -w "+ self.filename)
